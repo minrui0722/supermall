b066e7953ff20200878fdc0c3294998e9fedb25f
090199 <60974220+090199@users.noreply.github.com>
2020-06-15 09:27:38 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request(config) {
   //1.创建axios实例
   const instance=axios.create({
-    baseURL:'http://123.207.32.32:8000/api/m3',
+    baseURL:'http://123.123.123.123:1231/aaa',
     timeout:5000
   })
   //2.拦截器
